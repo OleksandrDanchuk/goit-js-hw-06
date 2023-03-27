8a06fec1af48be804a51c1caf0d6d44be3c9eb68
OleksandrDanchuk <117369013+OleksandrDanchuk@users.noreply.github.com>
2023-03-27 16:19:02 +0300

    firsttwo

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const listEl = document.querySelectorAll('.item');
+
+console.log('Number of categories: ', listEl.length);
+
+listEl.forEach(el => {
+  console.log('Category: ', el.firstElementChild.textContent);
+  console.log('Elements: ', el.lastElementChild.children.length);
+});
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulIngredients = document.querySelector('ul');
+
+const createUl = ingredients.map(ingredient => {
+  const liElement = document.createElement('li');
+  liElement.classList.add('item');
+  liElement.textContent = ingredient;
+  return liElement;
+});
+
+ulIngredients.append(...createUl);
